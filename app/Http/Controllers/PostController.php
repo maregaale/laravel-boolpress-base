30e6e98da86121b9cec9be48f086a2f7f4196371
Alessandro Marega <ale_marega@hotmail.it>
2021-05-24 17:14:34 +0200

    aggiunto seeder per i posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+
     }
 
     /**
@@ -44,9 +45,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        //
+        
     }
 
     /**
