0ae359218c4ddaad200f6b7781826cfb3a0de0c2
Alessandro Marega <ale_marega@hotmail.it>
2021-05-26 15:57:25 +0200

    aggiunta tabella tags con relazione con 'posts'

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Tag;
+use Illuminate\Support\Str;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = [
+            "html",
+            "css",
+            "js",
+            "php",
+            "laravel",
+            "vuejs",
+            "bootstrap",
+            "jquery",
+            "sql",
+        ];
+
+        foreach ($tags as  $tag) {
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->slug = Str::slug($tag, '-');
+            $newTag -> save();
+        }
+
+    }
+}
